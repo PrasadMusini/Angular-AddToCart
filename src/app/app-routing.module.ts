d7e8d6bc998d6743895c3ac41c2095f474f1af8e
PrasadMusini <prasadmusini1999@gmail.com>
2023-03-16 16:14:00 +0530

    commited by Prasad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ProductComponent } from './component/product/product.component';
+import { DemoComponent } from './demo/demo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'header',component:HeaderComponent},
+  {path:'products',component:ProductComponent},
+  {path:'cart',component:CartComponent},
+  {path:'demo',component:DemoComponent},
+  {path:'','redirectTo':'/products','pathMatch':'full'}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductComponent } from './component/product/product.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { DemoComponent } from './demo/demo.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductComponent,
+    DemoComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
